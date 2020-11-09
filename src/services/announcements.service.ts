707289f7faaf30762028faafd777495169353f21
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2020-11-09 18:39:39 -0500

    lint:fix
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- src/services/announcements.service.ts
+++ src/services/announcements.service.ts
@@ -3,18 +3,18 @@ import {Bot} from '../bot';
 import {Utils} from '../utils/utils';
 
 export namespace Announcements {
-    export function getNotifEmbed(msg: Message) {
-      const mess = msg.content;
-      const preview = (mess.length>80?mess.substr(0, 77)+'...':mess);
-      const notif = Utils.getDefEmbed()
-        .setTitle('Announcement Notification')
-        .setThumbnail(msg.author.displayAvatarURL())
-        .setDescription(`You may unsubscribe by running \`unsub\`
+  export function getNotifEmbed(msg: Message) {
+    const mess = msg.content;
+    const preview = (mess.length>80?mess.substr(0, 77)+'...':mess);
+    const notif = Utils.getDefEmbed()
+      .setTitle('Announcement Notification')
+      .setThumbnail(msg.author.displayAvatarURL())
+      .setDescription(`You may unsubscribe by running \`unsub\`
 on the SAC Discord Server.`)
-        .addField('Preview:', '```\n'+preview+'\n```')
-        .setFooter(`Announcement from ${msg.member.displayName}`,
-          Bot.api.user.displayAvatarURL())
-        .setTimestamp();
-      return notif;
-    }
+      .addField('Preview:', '```\n'+preview+'\n```')
+      .setFooter(`Announcement from ${msg.member.displayName}`,
+        Bot.api.user.displayAvatarURL())
+      .setTimestamp();
+    return notif;
+  }
 }
